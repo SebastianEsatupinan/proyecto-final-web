f2cb2926a56011ca0ee864b0d6097125f1a97cbc
Alejandro Sarria Diaz <alejandro.sarria@uao.edu.co>
2024-05-29 16:01:29 -0500

    implementacion clase firebase

---
+++ src/Componentes/firebase.jsx
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDnZhOdhx_togRQZ6c1cB-Bc9mKvLCIAZk",
+  authDomain: "proyecto-final-web-e4998.firebaseapp.com",
+  projectId: "proyecto-final-web-e4998",
+  storageBucket: "proyecto-final-web-e4998.appspot.com",
+  messagingSenderId: "756259000794",
+  appId: "1:756259000794:web:1f7d18a240c0601373f0e3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default firebase; 
